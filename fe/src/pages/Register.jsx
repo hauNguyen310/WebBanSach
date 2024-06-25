6ba5e40c9bd96145a53498bd33c797a5a3f781e7
NguyenThiHien6 <20130257@st.hcmuaf.edu.vn>
2024-06-25 22:54:48 +0700

    update word+ file jar

--- fe/src/pages/Register.jsx
+++ fe/src/pages/Register.jsx
@@ -43,10 +43,7 @@ const handleSubmit = async (e) => {
     try {
         const response = await axios.post('http://localhost:8080/register', formData);
         console.log(response.data);
-        setError('Đăng ký thành công');
-        //luu tt user
-        localStorage.setItem('auth-data',JSON.stringify(response.data));
-        // Xử lý khi đăng ký thành công
+        setError(response.data);
     } catch (error) {
         setError('Đăng ký không thành công');
     }
